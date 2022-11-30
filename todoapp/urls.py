afb6d457de020e9d397c0b7d2ef0cd6a88f28ffa
take917 <1tktk2tk3@gmail.com>
2022-11-30 11:30:42 +0900

    add path

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -1,5 +1,12 @@
 from django.urls import path
 from . import views
+from .views import TaskList,TaskDetail,TaskCreate,TaskUpdate,TaskDelete
+
+
 urlpatterns =[
-    path("",views.taskList)
+    path("",TaskList.as_view(),name="tasks"),
+    path("task/<int:pk>/",TaskDetail.as_view(),name="task"),
+    path("create-task/",TaskCreate.as_view(),name="create-task"),
+    path("edit-task/<int:pk>",TaskUpdate.as_view(),name="edit-task"),
+    path("delete-task/<int:pk>",TaskDelete.as_view(),name="delete-task")
 ]
